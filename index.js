71663ff74062e50e9a760cb307412a12b680ab34
Sharonne24 <sawuor395@gmail.com>
2023-03-20 14:10:22 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(a,b) {
+    let result=a+b
+    return result
+}
+
+function subtract(a,b) {
+    let result=a-b
+    return result
+}
+
+function multiply(a,b) {
+    let result=a*b
+    return result
+}
+
+function divide(a,b) {
+    let result=a/b
+    return result
+}
+
+function increment(n) {
+    let result=n+1
+    return result
+}
+
+function decrement(n) {
+    let result=n-1
+    return result
+}
+
+function makeInt(n) {
+    let result=n.toString()
+    return result
+}
+
+function makeInt(n) {
+    let integer=n
+    return integer
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+function makeInt(n) {
+    return parseInt(n,10);
+}
